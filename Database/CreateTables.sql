15499a9bc5ffe15a9c35fad1abbe528da7daf830
Lữ Nguyên Vĩnh Phúc <118715549+ITCSIU21217@users.noreply.github.com>
2024-05-06 12:37:10 +0700

    Create CreateTables.sql

---
+++ Database/CreateTables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Hotel (
+    HotelID INT PRIMARY KEY,
+    Name VARCHAR(255),
+    Address VARCHAR(255),
+    Phone VARCHAR(15),
+    Email VARCHAR(255),
+    Stars INT,
+);
+CREATE TABLE RoomType (
+    TypeID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Description VARCHAR(255),
+    PricePerNight DECIMAL(10, 2),
+    Capacity INT
+);
+CREATE TABLE Room (
+    RoomNumber INT PRIMARY KEY,
+    Hotel_id INT,
+    TypeID INT,
+    Status VARCHAR(20),
+    FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID),
+    FOREIGN KEY (TypeID) REFERENCES RoomType(TypeID)
+);
+CREATE TABLE Guest (
+    GuestID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    Address VARCHAR(255),
+    Phone VARCHAR(15),
+    Email VARCHAR(255)
+);
+CREATE TABLE Booking (
+    BookingID INT PRIMARY KEY,
+    GuestID INT,
+    RoomNumber INT,
+    CheckinDate DATE,
+    CheckoutDate DATE,
+    TotalPrice DECIMAL(10, 2),
+    FOREIGN KEY (GuestID) REFERENCES Guest(GuestID),
+    FOREIGN KEY (RoomNumber) REFERENCES Room(RoomNumber)
+);
+CREATE TABLE Payment (
+    PaymentID INT PRIMARY KEY,
+    BookingID INT,
+    Amount DECIMAL(10, 2),
+    PaymentDate DATE,
+    PaymentMethod VARCHAR(50),
+    FOREIGN KEY (BookingID) REFERENCES Booking(BookingID)
+);
