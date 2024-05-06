6cb762afd82ca13eae244b0f850b78a215a3b7f0
HƯNG TRẦN <hungtran@192.168.2.12>
2024-05-06 20:50:56 +0700

    fix the panel

--- src/ControlManager/Login.java
+++ src/ControlManager/Login.java
@@ -2,33 +2,46 @@ package ControlManager;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import Object.LoadSave;
+
+
 public class Login extends JFrame implements ActionListener {
     Login() {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
+        // Load the New Romance font
+//        Font newRomanceFont = new Font("New Romance", Font.PLAIN, 20); // Adjust size as needed
+
+
+        JLabel page = new JLabel("<html><b><font size='5'>Login Page</font></b></html>");
+//        page.setFont(newRomanceFont); // Set the font
+        page.setBounds(130,20,150,50);
+        add(page);
+
         JLabel user = new JLabel("Username");
-        user.setBounds(40,20,100,30);
+        user.setBounds(40,80,100,30);
         add(user);
 
         JTextField username = new JTextField();
-        username.setBounds(150,20,150,30);
+        username.setBounds(150,80,150,30);
         add(username);
 
         JLabel pass = new JLabel("Password");
-        pass.setBounds(40,70,100,30);
+        pass.setBounds(40,120,100,30);
         add(pass);
 
         JTextField password = new JTextField();
-        password.setBounds(150,70,150,30);
+        password.setBounds(150,120,150,30);
         add(password);
 
         JButton login = new JButton("Login");
-        login.setBounds(40,150,120,30);
+        login.setBounds(40,190,120,30);
         login.setBackground(Color.BLACK);
         add(login);
 
         JButton cancel = new JButton("Cancel");
-        cancel.setBounds(180,150,120,30);
+        cancel.setBounds(180,190,120,30);
         cancel.setBackground(Color.BLACK);
         add(cancel);
 
@@ -42,14 +55,15 @@ public class Login extends JFrame implements ActionListener {
         });
 
         ImageIcon login_page = new ImageIcon(ClassLoader.getSystemResource("5451_ho_00_p_2048x1536.jpg"));
-        Image i2 = login_page.getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT);
+        Image i2 = login_page.getImage().getScaledInstance(300,300,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
-        image.setBounds(330,10,260,260);
+        image.setBounds(370,10,290,290);
         add(image);
 
-        setBounds(350,180,700,650);
+        setBounds(350,180,680,370);
         setVisible(true);
+
     }
     public static void main (String[] args)
     {
