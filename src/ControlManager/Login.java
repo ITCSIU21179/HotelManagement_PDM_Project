bf89edc6897c2e3449436da6762efa10a5bae0b7
HƯNG TRẦN <hungtran@HUNGs-MacBook-Air.local>
2024-05-13 11:31:42 +0700

    fix error login page

--- src/ControlManager/Login.java
+++ src/ControlManager/Login.java
@@ -8,28 +8,28 @@ public class Login extends JFrame {
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
         JLabel user = new JLabel("Username");
-        user.setBounds(40,20,100,30);
+        user.setBounds(40,80,100,30);
         add(user);
 
         JTextField username = new JTextField();
-        username.setBounds(150,20,150,30);
+        username.setBounds(150,80,150,30);
         add(username);
 
         JLabel pass = new JLabel("Password");
-        pass.setBounds(40,70,100,30);
+        pass.setBounds(40,120,100,30);
         add(pass);
 
         JTextField password = new JTextField();
-        password.setBounds(150,70,150,30);
+        password.setBounds(150,120,150,30);
         add(password);
 
         JButton login = new JButton("Login");
-        login.setBounds(40,150,120,30);
+        login.setBounds(40,190,120,30);
         login.setBackground(Color.BLACK);
         add(login);
 
         JButton cancel = new JButton("Cancel");
-        cancel.setBounds(180,150,120,30);
+        cancel.setBounds(180,190,120,30);
         cancel.setBackground(Color.BLACK);
         add(cancel);
 
@@ -52,13 +52,22 @@ public class Login extends JFrame {
             }
         });
         ImageIcon login_page = new ImageIcon(ClassLoader.getSystemResource("5451_ho_00_p_2048x1536.jpg"));
-        Image i2 = login_page.getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT);
+        Image i2 = login_page.getImage().getScaledInstance(300,300,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image = new JLabel(i3);
-        image.setBounds(330,10,260,260);
+        image.setBounds(370,10,290,290);
         add(image);
 
         setBounds(350,180,700,650);
+        setVisible(true);
+
+        JLabel text = new JLabel("HOTEL MANAGEMENT SYSTEM");
+        text.setBounds(20,200,100,30);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("serif", Font.PLAIN,50));
+        add(text);
+
+        setBounds(340,180,680,370);
         setVisible(true);
     }
 }
