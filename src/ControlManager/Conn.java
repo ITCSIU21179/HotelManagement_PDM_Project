ea11ee6c4a7fb24193c91258059a8460a2b7ece8
ITCSIU21179 <118797773+ITCSIU21179@users.noreply.github.com>
2024-05-13 12:13:55 +0700

    HotelManagement phase 1

--- src/ControlManager/Conn.java
+++ src/ControlManager/Conn.java
@@ -1,14 +1,15 @@
 package ControlManager;
+
 import java.sql.*;
 
-public class Connection {
+public class Conn {
     Connection c;
     Statement s;
-    public Connection(){
+    public Conn(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = (Connection) DriverManager.getConnection("jdbc:mysql:///hotel_management_system", "root", "123456789");
-//            s = c.createStatement();
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel_management_system", "root", "123456789");
+            s = c.createStatement();
         } catch (Exception e) {
             e.printStackTrace();
         }
