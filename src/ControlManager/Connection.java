f850a037055cdaaae928f2d0672e30c9d0e889f5
ITCSIU21179 <118797773+ITCSIU21179@users.noreply.github.com>
2024-05-13 11:14:32 +0700

    HotelManagement phase 1

---
+++ src/ControlManager/Connection.java
@@ -0,0 +1,15 @@
+//package ControlManager;
+//import java.sql.*;
+//
+//public class Connection {
+//    public Connection(){
+//        Connection c;
+//        Statement s;
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            c = DriverManager.getConnection("jdbc:mysql:///hotel_management_system", "root", "");
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
