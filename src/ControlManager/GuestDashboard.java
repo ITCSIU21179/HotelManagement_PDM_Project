69ba663bbedbf277b4ba11c6882bfce84b613da9
ITCSIU21179 <118797773+ITCSIU21179@users.noreply.github.com>
2024-05-24 14:38:59 +0700

    HotelManagement final 1

--- src/ControlManager/GuestDashboard.java
+++ src/ControlManager/GuestDashboard.java
@@ -8,10 +8,13 @@ import java.sql.ResultSet;
 
 import net.proteanit.sql.*;
 
+import static ControlManager.Login.getGuest_id;
+
 public class GuestDashboard extends JFrame {
     JTable table;
     JTextField submit_room;
-    JButton submit;
+    JButton submit, availableRoom, bookedRoom;
+    String Guest_id = getGuest_id();
 
     public GuestDashboard()  {
         setBounds(0,0,1000, 700);
@@ -40,19 +43,49 @@ public class GuestDashboard extends JFrame {
         submit.setBounds(530,500,120,40);
         submit.setBackground(Color.WHITE);
         add(submit);
-//        String[] header = {"Available room", "Room ID", "Room Type", "Description","Price per night", "Capacity", "Choose Room"};
+
+        availableRoom = new JButton("Available room");
+        availableRoom.setBounds(150,430,200,40);
+        availableRoom.setBackground(Color.WHITE);
+        add(availableRoom);
+
+        bookedRoom = new JButton("Your booked rooms");
+        bookedRoom.setBounds(360,430,200,40);
+        bookedRoom.setBackground(Color.WHITE);
+        add(bookedRoom);
+
         table = new JTable();
         JScrollPane sp = new JScrollPane(table);
         sp.setBounds(150,120,700,300);
         Conn c = new Conn();
-        try{
 
-            ResultSet rs = c.s.executeQuery("select Room_id, room.Type_name, Description, PricePerNight, Capacity from room inner join roomtype on room.Type_name = roomtype.Type_name where Status = 'empty' order by Room_id ");
-            table.setModel(DbUtils.resultSetToTableModel(rs));
-            add(sp);
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+        availableRoom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+
+                    ResultSet rs = c.s.executeQuery("select Room_id, room.Type_name, Description, PricePerNight, Capacity from room inner join roomtype on room.Type_name = roomtype.Type_name where Status = 'empty' order by Room_id ");
+                    table.setModel(DbUtils.resultSetToTableModel(rs));
+                    add(sp);
+                } catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        bookedRoom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    ResultSet rs = c.s.executeQuery("SELECT r.Room_id FROM booking as b inner join room as r on b.Room_id = r.Room_id where b.User_id = '"+Guest_id+"' and r.Status = 'occupied' ");
+                    table.setModel(DbUtils.resultSetToTableModel(rs));
+                    add(sp);
+                } catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+
 
         submit.addActionListener(new ActionListener() {
             @Override
